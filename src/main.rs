2ab094055edb925c36791060d73d3948aa2bc413
Jorrit Salverda <jorrit.salverda@lightyear.one>
2021-12-29 08:08:41 +0100

    send jarvis measurements to bigquery

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use jarvis_lib::bigquery_client::{BigqueryClient, BigqueryClientConfig};
+use jarvis_lib::model::{Measurement};
 use nats;
 
-fn main() -> std::io::Result<()> {
-  let nc = nats::connect("jarvis-nats")?;
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let bigquery_client_config = BigqueryClientConfig::from_env().await?;
+  let bigquery_client = BigqueryClient::new(bigquery_client_config);
 
-  let sub = nc.subscribe("jarvis")?;
-  for msg in sub.messages() {}
+  let nc = nats::connect("jarvis-nats")?;
+  let sub = nc.subscribe("jarvis-measurements")?;
+
+  for msg in sub.messages() {
+    let measurement: Measurement = serde_json::from_slice(&msg.data).unwrap();
+    bigquery_client.insert_measurement(&measurement).await?;
+  }
 
   Ok(())
 }
