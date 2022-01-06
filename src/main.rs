22bbfe9fc148ed90ba33e9031db3cb7e42577114
Jorrit Salverda <jorrit.salverda@gmail.com>
2022-01-06 11:11:12 +0100

    add bigquery_client; use nats_client from jarvis-lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-mod nats_config;
+mod bigquery_client;
 
-use jarvis_lib::bigquery_client::{BigqueryClient, BigqueryClientConfig};
+use crate::bigquery_client::{BigqueryClient, BigqueryClientConfig};
 use jarvis_lib::model::{Measurement};
-use nats_config::NatsConfig;
+use jarvis_lib::nats_client::{NatsClientConfig, NatsClient};
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let bigquery_client_config = BigqueryClientConfig::from_env().await?;
   let bigquery_client = BigqueryClient::new(bigquery_client_config);
 
-  let nats_config = NatsConfig::from_env().await?;
+  bigquery_client.init_table().await?;
 
-  let nc = nats::connect(&nats_config.host)?;
-  let sub = nc.queue_subscribe(&nats_config.subject, &nats_config.queue)?;
+  let nats_client_config = NatsClientConfig::from_env().await?;
+  let nats_client = NatsClient::new(nats_client_config);
+
+  let sub = nats_client.queue_subscribe()?;
 
   for msg in sub.messages() {
     let measurement: Measurement = serde_json::from_slice(&msg.data).unwrap();
